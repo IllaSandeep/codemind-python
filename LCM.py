dcafebda22f144c7f73b2602bf8f43212e7a0ed7
IllaSandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-06-27 21:28:54 +0530

    a,b=map(int,input().split()) max=max(a,b) while True:     if max%a==0 and max%b==0:         print(max)         break     max+=1

--- LCM.py
+++ LCM.py
@@ -1,10 +1,7 @@
 a,b=map(int,input().split())
-if a>b:
-    max=a
-else:
-    max=b
+max=max(a,b)
 while True:
     if max%a==0 and max%b==0:
         print(max)
         break
-    max=max+1
+    max+=1
