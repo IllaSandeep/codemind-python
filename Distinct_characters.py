4bcee13e9632898917fde816353ca8eeb40618a6
IllaSandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-07-02 19:52:10 +0530

    a=input() c=0 for i in range(0,26):     if chr(97+i) in a:         print(chr(97+i),end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,17 +1,5 @@
-n = input()
-n = n.lower()
-n = n.replace(" ","")
-s = []
-for i in range(0,len(n)):
-    c = n.count(n[i])
-    if c==1:
-        s.append(n[i])
-s.sort()
-s = str(s)
-s = s.replace(",","")
-s = s.replace("[","")
-s = s.replace("]","")
-s = s.replace(",","")
-s = s.replace("'","")
-s = s.replace(" ","")
-print(s)
+a=input()
+c=0
+for i in range(0,26):
+    if chr(97+i) in a:
+        print(chr(97+i),end='')
