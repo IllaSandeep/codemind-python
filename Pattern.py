94751ddaacb6880e40fe888d676704af74ae0f65
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-10-16 12:58:24 +0530

    n=int(input()) for i in range(n):     for j in range(n-(i+1)):         print(' ',end='')     for j in range(i+1):         print(j+1,end='')     for j in range(i-1,-1,-1):         print(j+1,end='')     print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,9 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(i,end=' ')
+for i in range(n):
+    for j in range(n-(i+1)):
+        print(' ',end='')
+    for j in range(i+1):
+        print(j+1,end='')
+    for j in range(i-1,-1,-1):
+        print(j+1,end='')
     print()
