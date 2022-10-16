be2c4e0b759bcb2310f438d60db4a5771092f0d2
Sandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-10-16 12:55:58 +0530

    n=int(input()) l='ABCDEFGHIJKLMNOPQRSTUVWXYZ' for i in range(n):     for j in range(n-(i+1)):         print(' ',end='')     for j in range(i+1):         print(l[j],end='')     for j in range(i-1,-1,-1):         print(l[j],end='')     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,8 +1,12 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==j or i==n+1-j:
-            print(n-i+1,end=' ')
-        else:
-            print(' ',end='')
+l='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for i in range(n):
+    for j in range(n-(i+1)):
+        print(' ',end='')
+    for j in range(i+1):
+        print(l[j],end='')
+    for j in range(i-1,-1,-1):
+        print(l[j],end='')
     print()
+    
+    
